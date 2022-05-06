e6d38ff6a534c96099c18d2198bee4f03c789493
sungwoopark95 <swpark149531@gmail.com>
2022-05-07 00:16:12 +0900

    update assignments

---
+++ 2022-1/CFDS1/homework4/search.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "graph.h"
+#include <map>
+using namespace std;
+
+void BFS(Vertex* start,
+    std::map<Vertex*, unsigned int> &distance);
+void DFS(Vertex* vertex, unsigned int &timestamp,
+    std::map<Vertex*, unsigned int> &discovery_time,
+    std::map<Vertex*, unsigned int> &finishing_time);
